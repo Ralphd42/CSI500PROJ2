4c2e5359d4babcc3704adaeada742fc1f3f2fb1d
ralphd42 <rdonato@albany.edu>
2020-03-08 22:29:29 -0400

    SUNDAY NIGHT

--- consumer.c
+++ consumer.c
@@ -15,8 +15,66 @@ void processFile(char * fname)
     char * line;
     while ((read = getline(&line, &len, fp)) != -1) 
     {
+        //fork pipe write to input
+        int pipeStat =0;
+        int fd[2];
+        pipeStat = pipe(fd);
+        if( pipeStat<0)
+        {
+            perror("Error creating pipe");
+            exit(EXIT_FAILURE);
+        }
+        pid_t pid;
+        pid = fork();
+        if(pid<0)
+        {
+            perror("Error creating FORK");
+            exit(EXIT_FAILURE);
+        }
+        if( pid>0)
+        {
+            pipeouput(fd);
+            int status;
+            pid = wait(&status);
+
+
+
+        }
+        if( pid==0)
+        {
+            // do fork proc
+            // test read lines and exit
+            void ReadPipe( fd);
+
+            exit(EXIT_SUCCESS);
+        }
+
         
 
 
+    }
+}
+
+void pipeouput (int fd[2])
+{
+    close (fd[0]);
+    int fdesc =dup2(fd[1], STDOUT_FILENO);
+    if(fdesc<0)
+    {
+        perror("pipeouput");
+        fprintf(stderr, "pipeouput %d", fdesc);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void ReadPipe( int fd[2])
+{
+    close(fd[1]);
+    int d =dup2(fd[0], STDIN_FILENO);
+    if( d<0)
+    {
+        perror("Failed to read Pipe");
+        fprintf(stderr,"Failed to dup = %d\n\n",d);
+        exit(1);
     }
 }
--- consumer.h
+++ consumer.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include<wait.h>
+#include <sys/wait.h>
 void processFile(char * fname);
+void pipeouput (int fd[2]);
+void ReadPipe( int fd[2]);
