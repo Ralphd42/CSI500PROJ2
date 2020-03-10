e50b05247fc8f71e32ffd5668b5d0f47b31e64b9
Ralph Donato <ralph.donato@nyserda.ny.gov>
2020-03-10 10:48:18 -0400

    AA

--- producer.c
+++ producer.c
@@ -1,7 +1,33 @@
 #include "producer.h"
 
+
+
+
+/*
+    1 take a file and a character as input
+    void * arg will hold a pointer to FileData
+    read file line by line
+*/
 void *readerThread( void *arg)
 {
+     
+
+
+    FileData * fd = (FileData *) arg;
+    FILE* fp = fopen(fd->FileNameDesc, "rb");
+    //ssize_t	getline (char **, size_t *, FILE *);
+    ssize_t read ;
+    size_t len;
+    char * line;
+    while ((read = getline(&line, &len, fp)) != -1) 
+    {
+        pthread_mutex_lock(&mutcharQueue);
+        
+
+        pthread_mutex_unlock(&mutcharQueue);
+
+    }
+
     pthread_exit(NULL);
 }
 
--- producer.h
+++ producer.h
@@ -7,5 +7,20 @@ void *characterThread( void *arg);
 void *toupperThread( void *arg);
 void *writerThread( void *arg);
 
+typedef struct filedata{
+    char toreplace;
+    char * FileNameDesc;
+
+} FileData;
+
 /* QUEUES */
+ char charQueue   [10];  // first queue read thread writes here characterThread reads here
+ char toUpperQueue[10];  // second quere charaterthread writes here toUpper thread will read from here
+ char writerQueue [10];  // third queue to upper will write here and writer will read here
+
+/*MUtexes*/
+pthread_mutex_t mutcharQueue    = PTHREAD_MUTEX_INITIALIZER;  // mutex for character Queue
+pthread_mutex_t muttoUpperQueue = PTHREAD_MUTEX_INITIALIZER;// mutex for toUpperQueue
+pthread_mutex_t MmutwriterQueue = PTHREAD_MUTEX_INITIALIZER;
+
 
