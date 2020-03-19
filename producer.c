f3be371ab9f5849df19c7de1f501fbc985a08c40
ralphd42 <ralph.donato@albany.edu>
2020-03-19 00:01:15 -0400

    got it

--- producer.c
+++ producer.c
@@ -18,13 +18,14 @@ void *readerThread( void *arg)
     cqp1 =0;  
     cqp2 =0;
     printf("\n1\n");
-    bool first =false;
+    bool first =true;
     FileData * fd = (FileData *) arg;
     printf( "\ndesc %s\n", fd->FileNameDesc);
     FILE* fp = fopen(fd->FileNameDesc, "rb");
     int chr; 
     do 
     {
+        //printf("\n@\n");
         pthread_mutex_lock(&mutcharQueue);
         if( cqp1 >= QueueLen )
         {
@@ -32,26 +33,129 @@ void *readerThread( void *arg)
         }
         if( cqp1 ==cqp2 && !first)
         {
+           // printf("\n*\n");
             pthread_mutex_unlock(&mutcharQueue);
+             usleep(1);
         }else
         {
             chr =getc(fp);
             if( chr!=EOF)
             {
+                //printf("\n!\n");
                 charQueue[cqp1++]=chr;
             }else
             {
+                //printf("\n<>\n");
                 running=false;
             }
+            //printf("\n-\n");
+            pthread_mutex_unlock(&mutcharQueue);
+            usleep(1);
+            first =false;
         }
-        first =false;
+        
         //pthread_t charTh; 
         //pthread_create(&charTh,NULL,characterThread ,(void *) NULL );
-        pthread_mutex_unlock(&mutcharQueue);
+        
             
     }
     while (running);
+    printf("\nDONE\n");
+     pthread_exit(NULL);
 }
+
+/*
+    The character thread component will scan the line and replace each
+	 blank character by the character supplied by the client. It will then pass the line to the toUpper thread through another queue of messages.
+
+    needed vars
+    cqp1 =0;  
+    cqp2 =0;
+
+
+
+*/
+
+void *characterThread( void *arg)
+{
+    cqp2 =0;
+    bool hasdata =false;
+    while (running || hasdata)
+    {
+        pthread_mutex_lock(&mutcharQueue  );
+        if( (cqp2-1) !=cqp1)
+        {
+            printf("%C",charQueue[cqp2]);  
+            charQueue[cqp2]='\0'; 
+            cqp2++;
+            if( cqp2>=QueueLen)
+            {
+                cqp2=0;
+            }
+
+
+        }else
+        {
+            hasdata=false;
+        }
+        pthread_mutex_unlock(&mutcharQueue);
+
+
+
+
+
+    }
+
+
+
+    
+    pthread_exit(NULL);
+}
+
+void *toupperThread( void *arg)
+{
+    pthread_exit(NULL);
+}
+
+void *writerThread( void *arg)
+{
+    pthread_exit(NULL);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -94,62 +198,5 @@ void *readerThread_old( void *arg)
 
     }
 
-    pthread_exit(NULL);
-}
-
-/*
-    The character thread component will scan the line and replace each
-	 blank character by the character supplied by the client. It will then pass the line to the toUpper thread through another queue of messages.
-
-    needed vars
-    cqp1 =0;  
-    cqp2 =0;
-
-
-
-*/
-
-void *characterThread( void *arg)
-{
-    cqp2 =0;
-    bool hasdata =false;
-    while (running || hasdata)
-    {
-        pthread_mutex_lock(&mutcharQueue  );
-        if( cqp2!=cqp1)
-        {
-            printf("%c",charQueue[cqp2]);    
-            cqp2++;
-            if( cqp2>=QueueLen)
-            {
-                cqp2=0;
-            }
-
-
-        }else
-        {
-            hasdata=false;
-        }
-        pthread_mutex_unlock(&mutcharQueue);
-
-
-
-
-
-    }
-
-
-
-    
-    pthread_exit(NULL);
-}
-
-void *toupperThread( void *arg)
-{
-    pthread_exit(NULL);
-}
-
-void *writerThread( void *arg)
-{
     pthread_exit(NULL);
 }
